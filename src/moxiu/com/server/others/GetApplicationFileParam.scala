ca71c9c922bb186a70fd3d52b66cd1365f22cbdf
chind <chind@chinddeMacBook-Pro.local>
2018-11-12 17:18:27 +0800

    添加通过@value注解获取application.properties文件中定义的变量

---
+++ src/moxiu/com/server/others/GetApplicationFileParam.scala
@@ -0,0 +1,20 @@
+package moxiu.com.server.others
+
+import org.springframework.web.bind.annotation.{ RequestMapping, RestController }
+import org.springframework.beans.factory.annotation.Value
+
+/**
+ * @author 宿荣全
+ * 通过注解@Value 获取Application.properties中定义的变量
+ * 注意：
+ * 通过注解@Value 获取时必需是类变量，写在方法中无法加载
+ */
+@RestController
+class GetApplicationFileParam {
+  @Value("${app.name}")
+  var app_Name: String = _
+  @Value("${moxiu.author}")
+  var author: String = _
+  @RequestMapping(Array("/aaaa", "/bbb"))
+  def fun() = "通过注解@Value 获取Application.properties中定义的变量：app.name=" + app_Name + "，moxiu.author" + author
+}
